85548ae6f3541955704bed07dd315636b532ae2c
bishwa221 <94813630+bishwa221@users.noreply.github.com>
2023-11-26 23:45:08 -0600

    Update image_process.cpp

--- image_process.cpp
+++ image_process.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 int main() {
-    // Read the input image
-    cv::Mat inputImage = cv::imread("input_image.jpg");
+    // Reading the input image
+    cv::Mat inputImage = cv::imread("input_image.jpg"); // change it to your local image // use blurred image for visible sharpening effect
 
     // Check if the image is loaded successfully
     if (inputImage.empty()) {
@@ -11,14 +11,14 @@ int main() {
         return -1;
     }
 
-    // Display the original image
+    // Displaying the original image
     cv::imshow("Original Image", inputImage);
     cv::waitKey(0);
 
     // Section 1: Sharpening, Edge Extraction (Canny), Resizing
     // Sharpen the image
     cv::Mat sharpened;
-    cv::Mat kernel = (cv::Mat_<float>(3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
+    cv::Mat kernel = (cv::Mat_<float>(3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0); // 3,3 kernel to convolve with
     cv::filter2D(inputImage, sharpened, inputImage.depth(), kernel);
 
     // Apply Canny edge detection
@@ -28,7 +28,7 @@ int main() {
 
     // Resize the image by half
     cv::Mat resized;
-    cv::resize(inputImage, resized, cv::Size(), 0.5, 0.5);
+    cv::resize(inputImage, resized, cv::Size(), 0.5, 0.5); // half the size
 
     // Section 2: Save the images to a file
     cv::imwrite("sharpened_image.jpg", sharpened);
